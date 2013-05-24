476ad55e653d19a7dd74e1c589f1e6d06f95e459
Ryan McCahan <ryan@mccahan.net>
2013-05-23 19:15:46 -0600

    Contact form using Contact Form 7

---
+++ wp/wp-content/themes/gradea/contact-form.php
@@ -0,0 +1,19 @@
+<div id="Morgan" class="reveal-modal large-12">
+  <h2>Contact Us</h2>
+  <div class="row">
+    <div class="columns large-4">
+      <p><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png"></p>
+      <p>3865 Wilson Boulevard<br>
+
+      Suite 300<br>
+      Arlington, VA 22205</p>
+      <p><a href="#">Follow</a><a href="#">Like</a></p>
+    </div>  
+    <div class="large-8 columns">
+      <?php echo do_shortcode( '[contact-form-7 id="83" title="Contact form 1"]'); ?>
+    </div>
+    <p><a class="close-reveal-modal">&#215;</a></p>
+      </div>
+    </div>
+  </div></div>
+</div>
--- wp/wp-content/themes/gradea/footer.php
+++ wp/wp-content/themes/gradea/footer.php
@@ -59,34 +59,7 @@
     $(document).foundation();
   </script>
 
-<div id="Morgan" class="reveal-modal large-12">
-  <h2>Contact Us</h2>
-  <div class="row"><div class="columns large-4">
-    <p><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png"></p>
-    <p>3865 Wilson Boulevard<br>
-
-      Suite 300<br>
-      Arlington, VA 22205</p>
-      <p><a href="#">Follow</a><a href="#">Like</a></p>
-    </div>  
-    <div class="large-8 columns">
-      <div class="columns small-6">Name*
-        <input type="text">
-        Email*
-        <input type="text"></div>
-        <div class="columns small-6">Company*
-          <input type="text"> Phone
-          <input type="text"></div>
-         
-            <div class="columns large-12">Message <textarea rows="3"></textarea>
-            <button href="#" class="right">Submit</button></div>
-          
-        </div>
-        <p><a class="close-reveal-modal">&#215;</a></p>
-      </div>
-    </div>
-  </div></div>
-</div>
+<?php get_template_part('contact-form'); ?>
 
 
 </body>
