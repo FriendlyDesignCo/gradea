29a0b1eb8cb4e984dd27d1375ae6ba7be4f89c87
Ross Nover <ross@friendlydesign.co>
2013-06-05 17:29:30 -0400

    Facebook like button fix (I hope)

--- wp/wp-content/themes/gradea/contact-form.php
+++ wp/wp-content/themes/gradea/contact-form.php
@@ -15,14 +15,16 @@
 
 
 
-      <p><div id="fb-root"></div>
+      <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=25146247312";
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script></p>
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-href="http://www.facebook.com/GradeAFresh?fref=ts" data-send="false" data-width="450" data-show-faces="true"></div>
+
     
 
 
