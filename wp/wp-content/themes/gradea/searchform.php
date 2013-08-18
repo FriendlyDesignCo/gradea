0df9467b58390cf5b48ebb844e8d7b2e2ff19cbe
Ross Nover <ross@friendlydesign.co>
2013-08-18 15:16:06 -0400

    Search

--- wp/wp-content/themes/gradea/searchform.php
+++ wp/wp-content/themes/gradea/searchform.php
@@ -1,7 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div><label class="screen-reader-text" for="s">Search for:</label>
-    	WORKING
+    <div><label class="screen-reader-text" for="s">Search:</label>
         <input type="text" value="" name="s" id="s" />
-        <input type="submit" id="searchsubmit" value="Search" />
+        <input type="submit" id="searchsubmit" value="GO" />
     </div>
 </form>
